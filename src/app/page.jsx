4c77c36f612356544ad5f7fd946df3d6e54b19ba
Tekalign Getachew <tekaligngetachew114@gmail.com>
2026-01-12 15:00:54 +0300

    Fix import statement for Contact component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 import About from "@/components/About";
-import Contact from "@/components/Contact";
+import Contact from "@/components/Contact"; 
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import Portfolio from "@/components/Portfolio";
