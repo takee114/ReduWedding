0bab78f280737423eda5455ca206585de39b2ea6
Tekalign Getachew <tekaligngetachew114@gmail.com>
2026-01-12 14:57:22 +0300

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 import About from "@/components/About";
-import Contact from "@/components/contact";
+import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import Portfolio from "@/components/Portfolio";
